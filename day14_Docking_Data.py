0e40a83e69ccf5d2cd89d5e3697e7c4128aef44b
jha-vikas <vikasjhanitk@gmail.com>
2020-12-24 13:54:13 +0530

    Adding Day14 files. Both part1 & part2 solutions

--- day14_Docking_Data.py
+++ day14_Docking_Data.py
@@ -2,6 +2,7 @@
 
 import re
 from typing import Dict, List
+import itertools
 
 EX_INPUT = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
@@ -41,7 +42,7 @@ def run(lines: List[str]) -> int:
             #temp_list.append(temp_dict)
             #temp_dict = {}
             _, mask = line.split(" = ")
-            print(i, mask)
+            #print(i, mask)
             mask = mask.rstrip().strip()
         elif line.startswith("mem"):
             memory, val = line.split(" = ")
@@ -53,6 +54,59 @@ def run(lines: List[str]) -> int:
     temp_list.append(temp_dict)
     return total
     
+def binary_combo(binary_str: str) -> List:
+    if binary_str.find("X") == -1:
+        return binary_str
+    else:
+        all_combs = []
+        ind = binary_str.find("X")
+        to_return = [(binary_str[:ind]+i+binary_str[(ind+1):]) for i in ["0","1"]]
+        all_combs.extend([binary_combo(words) for words in to_return])
+        return all_combs
+       
+
+def apply_mask2(mask: str, mem_loc: int):
+    binary_str = bin(mem_loc)[2:]
+    binary_str = binary_str.rjust(36, "0")
+    binary_str = list(binary_str)
+
+    for i, (digit , m) in enumerate(zip(binary_str, mask)):
+        if m == "X" or m == "1":
+            binary_str[i] = m
+    binary_str = "".join(binary_str)
+    #X_indices = [i for i,j in enumerate(binary_str) if j == "X"]
+    all_combo = binary_combo(binary_str)
+    while not all([isinstance(i, str) for i in all_combo]):
+        all_combo =  list(itertools.chain(*all_combo))
+    return [int(i,2) for i in all_combo]
+
+
+def run2(lines: List[str]) -> int:
+    total = 0
+    mask  = None
+    temp_list = []
+    temp_dict = {}
+    i = 0
+    for line in lines:
+        if line.startswith("mask"):
+            #total += sum(temp_dict.values())
+            #temp_list.append(temp_dict)
+            #temp_dict = {}
+            _, mask = line.split(" = ")
+            #print(i, mask)
+            mask = mask.rstrip().strip()
+        elif line.startswith("mem"):
+            memory, val = line.split(" = ")
+            memory = int(memory[4:-1])
+            new_mem_loc = apply_mask2(mask, memory)
+            val = int(val.rstrip())
+            for loc in new_mem_loc:
+                temp_dict[loc] = val
+        i += 1
+    total += sum(temp_dict.values())
+    temp_list.append(temp_dict)
+    return total
+
 
 if __name__ == "__main__":
     with open("day14_input.txt") as f:
